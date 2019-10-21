1862db7cebec4b892c7288152a1ddaf95da59b7a
anderslindho <anders.lindholsson@esss.se>
2019-10-21 21:39:47 +0200

    First commit, record and playback single wav

---
+++ playback.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+
+import pyaudio
+import wave
+
+def play_sound(key):
+    filename = key + '.wav'
+    chunk = 1024  # Set chunk size of 1024 samples per data frame
+    wf = wave.open(filename, 'rb') # Open the sound file 
+    p = pyaudio.PyAudio()
+
+    # Open a .Stream object to write the WAV file to
+    stream = p.open(format = p.get_format_from_width(wf.getsampwidth()),
+                    channels = wf.getnchannels(),
+                    rate = wf.getframerate(),
+                    output = True)
+
+    data = wf.readframes(chunk) # Read data in chunks
+
+    # Play the sound by writing the audio data to the stream
+    while data != '':
+        stream.write(data)
+        data = wf.readframes(chunk)
+
+    stream.close()
+    p.terminate()
+
+def main():
+    play_sound('output')
+
+main()
