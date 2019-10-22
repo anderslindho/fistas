343529ccd029111bb21b1bdfb06593d4b792da85
anderslindho <anders.lindholsson@esss.se>
2019-10-22 23:12:45 +0200

    tidying up

--- record.py
+++ record.py
@@ -5,50 +5,45 @@ import pyaudio
 import wave
 
 def record_sound(key):
-    chunk = 1024  # Samples per chunk
-    sample_format = pyaudio.paInt16  # 16 bits per sample
-    channels = 2 # Stereo
-    fs = 44100  # Record at 44100 samples per second
-    seconds = 3
+    chunk = 4096  # samples per chunk
+    sample_format = pyaudio.paInt16  # bits per sample (ADC val)
+    channels = 2 # mono/stereo
+    freq = 22050  # samples per second
+    clip_length = 3 # seconds
     filename = key + '.wav'
     frames = []
     
     p = pyaudio.PyAudio()
     print(f'Recording {key}')
     time.sleep(0.3)
-
     stream = p.open(format=sample_format,
                     channels=channels,
-                    rate=fs,
+                    rate=freq,
                     frames_per_buffer=chunk,
                     input=True)
-
-
-    # Store data in chunks for 3 seconds
-    for i in range(0, int(fs / chunk * seconds)):
+    for i in range(0, int(freq / chunk * clip_length)):
         data = stream.read(chunk)
         frames.append(data)
-
     stream.stop_stream()
     stream.close()
     p.terminate()
 
-    print('Finished recording')
+    print('Finished recording, saving to file...')
 
     wf = wave.open(filename, 'wb')
     wf.setnchannels(channels)
     wf.setsampwidth(p.get_sample_size(sample_format))
-    wf.setframerate(fs)
+    wf.setframerate(freq)
     wf.writeframes(b''.join(frames))
     wf.close()
 
 def main():
     keys = []
-
     with open('mappings.conf', 'r') as f:
         for line in f:
             keys.append(line.strip())
 
+    print(f'Will record ')
     for key in keys:
         record_sound(key)
 
