1b2a2ca7ae193d657ba09a6ea9b6b6cb0628202e
anderslindho <anders.lindholsson@esss.se>
2019-10-22 22:57:56 +0200

    first working version

--- playback.py
+++ playback.py
@@ -1,35 +1,18 @@
 #!/usr/bin/env python3
 
 import numpy as np
-import time
-import pyaudio
-import wave
 import pygame
 
-def play_sound(key, p):
+def play_sound(key):
+    pygame.mixer.init(frequency=22050, size=-16, channels=2, buffer=4096)
     filename = key + '.wav'
-    wf = wave.open(filename, 'r')
-    
-    def callback(in_data, frame_count, time_info, flag):
-        data = wf.readframes(frame_count)
-        return (data, pyaudio.paContinue)
-
-    stream = p.open(format = p.get_format_from_width(wf.getsampwidth()),
-                    channels = wf.getnchannels(),
-                    rate = wf.getframerate(),
-                    output = True,
-                    stream_callback=callback)
-
-    stream.start_stream()
-    while stream.is_active():
-        time.sleep(0.1)
-
-    stream.stop_stream()
-    stream.close()
+    sound = pygame.mixer.Sound(filename)
+    snd_array = pygame.sndarray.array(sound)
+    snd_out = pygame.sndarray.make_sound(snd_array)
+    snd_out.play()
 
 def main():
     keys = []
-    p = pyaudio.PyAudio()
 
     with open('mappings.conf', 'r') as f:
         for line in f:
@@ -41,14 +24,12 @@ def main():
         event = pygame.event.wait()
         if event.type == pygame.KEYDOWN:
             key = pygame.key.name(event.key)
-            print(key)
 
             if event.key == pygame.K_ESCAPE:
                 pygame.quit()
-                p.terminate()
                 raise KeyboardInterrupt
             elif key in keys:
-                play_sound(key, p)
+                play_sound(key)
 
 
 if __name__ == '__main__':
